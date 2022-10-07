13f25b1cc265e22ad24c5ca09f39e776134ab317
Vsimpro <jsimrpo@gmail.com>
2022-10-07 16:53:35 +0300

    make js dynamic

--- js/script.js
+++ js/script.js
@@ -1,34 +1,30 @@
+// Global Variables
 var list = [];
 var maxAmount = 8
 
-function addItemToJSon(element){
-    list.push(element)
-    localStorage.setItem("items", JSON.stringify(list));
-}
-
-function removeFromJSon(element){
-    var items = JSON.parse(localStorage.items)
-    for (let i = 0; i < items.length; i++) {
-        if (element == items[i]) {
-            delete items[i];
-            delete list[i];
-        }
-    }
-    localStorage.setItem("items", JSON.stringify(items));
-}
-
-function onLoad() {
+// Prepare onload
+window.onload = function(){ 
     // get old items
     if (localStorage.items != undefined) {
         let oldItems = JSON.parse(localStorage.items)
-	for (let i = 0; i < oldItems.length; i++) {
-	    if (oldItems[i] == null) {
-	       continue
-	    }
-	    addNewItem(oldItems[i])
-	    list.push(oldItems[i])
+
+        for (let i = 0; i < oldItems.length; i++) {
+            if (oldItems[i] == null) {
+            continue
+            }
+            
+            // Edge case where local storage has been fiddled with (idk why you would)
+            if (inputValidation(oldItems[i])) {
+                addNewItem(oldItems[i])
+                list.push(oldItems[i])
+            } else {
+                alert("Problem with localstorage.")
+                clearAll()
+            }
         }
     }
+    // add event listener to 
+    document.getElementById("main").addEventListener("click", getTask);
 
     // pressing enter triggers the "+" button;
     let taskbox = document.getElementById("task")
@@ -42,16 +38,35 @@ function onLoad() {
    // create "clear tasks" button
    var clearall = document.getElementById("clear");
    clearall.onclick = clearAll;
+};
+
+
+// Functions
+function addToLocal(element){
+    list.push(element)
+    localStorage.setItem("items", JSON.stringify(list));
+}
+
+function removeFromLocal(element){
+    var items = JSON.parse(localStorage.items)
+    for (let i = 0; i < items.length; i++) {
+        if (element == items[i]) {
+            delete items.splice(i, 1);
+            delete list.splice(i, 1);
+        }
+    }
+    localStorage.setItem("items", JSON.stringify(items));
 }
 
 function clearAll() {
+    if (list.length == 0) { return }
     // Are you sure? alert
     if (!confirm("Are you sure you want to delete all the tasks?")) {
         return
     }
     // clear the JSON
     for (var i = 0; i < list.length; i++) {
-        removeFromJSon(list[i]);
+        removeFromLocal(list[i]);
     }
     // clear the <ul>
    document.getElementById("tasks").innerHTML = "";
@@ -68,7 +83,7 @@ function doTask() {
 
 function removeTask() {
     let element = this.parentElement;
-    removeFromJSon(element.textContent.replace("doneremove", ""))
+    removeFromLocal(element.textContent.replace("doneremove", ""))
     document.getElementById("limit").style.display = "none"
     element.remove();
 }
@@ -102,36 +117,41 @@ function addNewItem(input) {
     document.getElementById("tasks").appendChild(listItem)
 }
 
-function getTask() {
-    // get the input value
-    var inputValue = document.getElementById("task").value;
+function inputValidation(inputValue) {
     var errorLine = document.getElementById("task");
+    errorLine.style["border"] = "2px dashed red";
 
     // input validation
     if (inputValue.length >= 20) {
-	document.getElementById("toolong").style.display = "";
-	errorLine.style["border"] = "2px dashed red";
-        return;
+        document.getElementById("toolong").style.display = "";
+        return false;
     }
-
+        
     if (inputValue.length <= 3) {
         document.getElementById("error").style.display = ""
-        errorLine.style["border"] = "2px dashed red";
-	return;
+        return false;
     }
-
+        
     if (list.length >= maxAmount) {
         document.getElementById("limit").style.display = ""
-	errorLine.style["border"] = "2px dashed red"
-    } else {
-        addNewItem(inputValue)
-        addItemToJSon(inputValue)
+        return false;
     }
-
 
     // clear the value & error
     document.getElementById("task").value = "";
     document.getElementById("error").style.display = "none"
     document.getElementById("toolong").style.display = "none";
     errorLine.style["border"] = "";
+
+    return true;
+}
+
+function getTask() {
+    // get the input value
+    var inputValue = document.getElementById("task").value;
+    
+    if (inputValidation(inputValue)) {
+        addNewItem(inputValue)
+        addToLocal(inputValue)
+    }
 }
